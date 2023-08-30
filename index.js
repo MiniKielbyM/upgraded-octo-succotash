a654bcee7c94e8fcf2a38ef9e8851b520585b6b3
Michael Kielbasinski <85803354+MiniKielbyM@users.noreply.github.com>
2023-08-30 15:25:04 +0000

    base app finished

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+    console.log(msg)
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
