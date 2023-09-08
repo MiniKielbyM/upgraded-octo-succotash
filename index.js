a7843b23408952ea379426a065bac105fe4b1cf9
Michael Kielbasinski <85803354+MiniKielbyM@users.noreply.github.com>
2023-09-08 13:40:33 +0000

    more dependencies

--- index.js
+++ index.js
@@ -1,50 +1,56 @@
-
-var express = require('express');
+const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-var md = require('markdown-it')();
+const md = require('markdown-it')();
 const port = process.env.PORT || 3030;
-app.use(express.static('public'))
+const fileUpload = require('express-fileupload'); // Require express-fileupload
+const path = require('path'); // Added for file handling
+const fs = require('fs'); // Added for file handling
+
+app.use(express.static('public'));
 app.use("/auth", express.static(__dirname + "/login.html"));
+
+// Use the fileUpload middleware
+app.use(fileUpload());
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
-io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-console.log('chat msg received on server from client')
-    outmsg = [msg[0], md.render(msg[1]), msg[2]]
-    io.emit('chat message', outmsg);
-    console.log(msg)
-    console.log(outmsg)
+
+// Add a route for handling image uploads
+app.post('/upload', (req, res) => {
+  // Handle the image upload here
+  const uploadDir = path.join(__dirname, 'uploads'); // Directory where uploaded images will be stored
+
+  // Create the 'uploads' directory if it doesn't exist
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  const upload = req.files.image; // 'image' should match the field name in the HTML form
+
+  // Move the uploaded image to the 'uploads' directory
+  upload.mv(path.join(uploadDir, upload.name), (err) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    // Respond with a success message or other relevant response
+    res.send('Image uploaded successfully');
   });
 });
 
-/*const DescopeClient = require('@descope/node-sdk');
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    console.log('chat msg received on server from client');
+    outmsg = [msg[0], md.render(msg[1]), msg[2]];
+    io.emit('chat message', outmsg);
+    console.log(msg);
+    console.log(outmsg);
+  });
+});
 
-let descopeClient;
-
-try {
-    descopeClient = DescopeClient({ projectId: 'P2UyYHH8xdJz6jgOAk3SzVbawVyg' });
-} catch (error) {
-    console.log("failed to initialize: " + error)
-}
-
-// Fetch session token from HTTP Authorization Header
-const sessionToken = "xxxx"
-
-async function validateSession() {
-  try {
-    const authInfo = await descopeClient.validateSession(sessionToken);
-    console.log("Successfully validated user session:");
-    console.log(authInfo);
-  } catch (error) {
-    console.log ("Could not validate user session " + error);
-  }
-}
-
-validateSession();
-*/
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
